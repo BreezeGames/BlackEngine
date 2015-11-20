0ace8d575822ea0b13dd1fc41d22d9180cca24a9
Георгий <popoff96@live.com>
2015-11-20 13:46:11 +0300

    Добавлен Doxygen в качестве системы документирования кода

---
+++ Docs/html/navtreedata.js
@@ -0,0 +1,14 @@
+var NAVTREE =
+[
+  [ "BlackEngine", "index.html", [
+    [ "BlackEngine", "md__c_1__users__xD0_x93_xD0_xB5_xD0_xBE_xD1_x80_xD0_xB3_xD0_xB8_xD0_xB9__documents__visual__studb51a1546a7803dfe1b81efa989471ad3.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ Docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['blackengine',['BlackEngine',['../md__c_1__users__xD0_x93_xD0_xB5_xD0_xBE_xD1_x80_xD0_xB3_xD0_xB8_xD0_xB9__documents__visual__studb51a1546a7803dfe1b81efa989471ad3.html',1,'']]]
+];
